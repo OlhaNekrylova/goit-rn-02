a502c49d7c6a4b658e7819bf8bfe6b600ec44e77
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-03-15 23:20:07 +0200

    added Screens

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+
+const backImage = require('./assets/favicon.png');
 
 export default function App() {
+  const [activeScreen, setActiveScreen] = useState(0);
+  const changeScreen = (value) => { setActiveScreen (value) }
+
   return (
+    // <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={backImage} style={styles.backImgage} >
+        { activeScreen === 0 ? <LoginScreen changeScrenn={ changeScreen }/> :
+          <RegistrationScreen changeScrenn={ changeScreen }/>}
+      </ImageBackground>
+      {/* <StatusBar style="auto" />   */}
     </View>
+  // </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  }, 
+  backImgage: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    width: '100%'
   },
 });
